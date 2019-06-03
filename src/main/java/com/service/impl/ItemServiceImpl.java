dc58f0e8731917be467517db5c221eea83fe8560
许杰仕 <@DESKTOP-EGTO3V1>
2019-06-03 17:32:33 +0800

    push

---
+++ src/main/java/com/service/impl/ItemServiceImpl.java
@@ -0,0 +1,34 @@
+package com.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.dao.AdminDao;
+import com.dao.ItemDao;
+import com.model.Admin;
+import com.model.Item;
+import com.service.ItemService;
+
+public class ItemServiceImpl implements ItemService {
+	@Autowired
+	private ItemDao itemDao;
+
+	public void setAdminDao(AdminDao adminDao) {
+		this.itemDao = itemDao;
+	}
+
+	@Override
+	public int addItem(Item item) {
+		// TODO Auto-generated method stub
+		return itemDao.addItem(item);
+	}
+
+	@Override
+	public List<Item> getItemByorderId(String orderId) {
+		// TODO Auto-generated method stub
+		return itemDao.getItemByorderId(orderId);
+	}
+
+	
+}
---
+++ src/test/java/com/test/ItemTest.java
@@ -0,0 +1,33 @@
+package com.test;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+import com.dao.ItemDao;
+
+import com.model.Item;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring-mybatis.xml")
+public class ItemTest {
+	@Autowired
+	private ItemDao itemDao;
+	
+	@Test
+	public void addItem(){
+		Item item=new Item("1",2,"茶叶蛋",15.0,3);
+		int addItem = itemDao.addItem(item);
+		System.out.println(addItem);
+	}
+	@Test
+	public void getItemByorderId(){
+		List<Item> item = itemDao.getItemByorderId("1");
+		System.out.println(item);
+	}
+}
