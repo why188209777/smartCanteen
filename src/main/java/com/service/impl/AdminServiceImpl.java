c53e69b1232a93aadce90ab723711cc21c23f96e
wzl <2469480096@qq.com>
2019-06-03 19:54:13 +0800

    commit wzl

--- src/main/java/com/service/impl/AdminServiceImpl.java
+++ src/main/java/com/service/impl/AdminServiceImpl.java
@@ -1,26 +1,26 @@
 package com.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.dao.AdminDao;
 import com.model.Admin;
 import com.service.AdminService;
 
+@Service
 public class AdminServiceImpl implements AdminService {
 
 	@Autowired
 	private AdminDao adminDao;
 
-
+	public void setAdminDao(AdminDao adminDao) {
+		this.adminDao = adminDao;
+	}
 
 	@Override
 	public Admin login(String aname, String password) {
 		// TODO Auto-generated method stub
 		return adminDao.login(aname, password);
-	}
-	
-	public static void main(String[] args) {
-		System.out.println(new AdminServiceImpl().login("马云", "123456"));
 	}
 
 }
--- src/test/java/com/test/AdminTest.java
+++ src/test/java/com/test/AdminTest.java
@@ -6,20 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.dao.AdminDao;
 import com.model.Admin;
 import com.service.AdminService;
+import com.service.impl.AdminServiceImpl;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:spring-mybatis.xml")
 public class AdminTest {
 
 	@Autowired
-	private AdminDao adminDao;
+	private AdminService adminService;
 	
 	@Test
 	public void login(){
-		Admin admin = adminDao.login("马云", "123456");
+		Admin admin = adminService.login("马云", "123456");
 		System.out.println(admin);
 	}
 	
