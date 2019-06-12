00893c92374375376c9f1dcfc97e45e5ae366a97
Fsp <1028719566@qq.com>
2019-06-12 17:18:12 +0800

    fsp commit

--- src/main/webapp/js/page/menu.js
+++ src/main/webapp/js/page/menu.js
@@ -3,54 +3,7 @@
  */
 $(function() {
 
-	//初始化餐厅列表
-	function getAllCanteen() {
-		$.ajax({
-			type: "post",
-			url: "http://localhost:8080/smartCanteen/canteen/getAllCanteen.do",
-			dataType: "json",
-			success: function(data) {
-				let dom = $("#canteenList");
-				dom.html("");
-				let str = ""; //通用字符串模板
-				for(let i = 0; i < data.length; i++) {
-					str += `
-						<option value="${i+1}">${data[i].cname}</option>
-					`;
-				}
-				dom.append(str);
-			},
-			error: function(error) {
-				console.log(error);
-			}
-		});
-	}
-
-	//初始化店铺列表
-	function getAllMerchant(cid) {
-		$.ajax({
-			type: "post",
-			url: "http://localhost:8080/smartCanteen/merchant/getMerchantByCid.do",
-			data: {
-				cid: cid
-			},
-			dataType: "json",
-			success: function(data) {
-				let dom = $("#storeList");
-				dom.html("");
-				let str = ""; //通用字符串模板
-				for(let i = 0; i < data.length; i++) {
-					str += `
-						<option value="${data[i].mid}">${data[i].mname}</option>
-					`;
-				}
-				dom.append(str);
-			},
-			error: function(error) {
-				console.log(error);
-			}
-		});
-	}
+	
 
 	//初始化商品列表
 	function getFoodByCanteenAndMerchant(cid, mid) {
@@ -71,8 +24,8 @@ $(function() {
 		});
 	}
 
-	getAllCanteen();
-	getAllMerchant(1);
+	/*getAllCanteen();
+	getAllMerchant(1);*/
 	//初始化进来第一家餐厅的第一家店铺
 	getFoodByCanteenAndMerchant(1, 1);
 	//点击餐厅改变店铺列表
@@ -99,7 +52,7 @@ $(function() {
 							<div>
 								<span class="item_price">
 									<a>
-										<span  style="cursor: pointer;" class="glyphicon glyphicon-shopping-cart" aria-hidden="true" data-foodImg="${data.list[i].image}" data-foodName="${data.list[i].fname}" data-foodPrice="${data.list[i].price}" data-foodNum="${data.list[i].number}"></span>
+										<span  style="cursor: pointer;" class="glyphicon glyphicon-shopping-cart" aria-hidden="true" data-foodImg="${data.list[i].image}" data-foodName="${data.list[i].fname}" data-foodPrice="${data.list[i].price}" data-foodNum="${data.list[i].number}" data-foodmid="${data.list[i].mid}"></span>
 									</a>
 								</span>
 							</div>
@@ -111,7 +64,7 @@ $(function() {
 									<span style="display:none">${data.list[i].image}</span>
 									<span class="foodName" style="margin-right: 30px;">${data.list[i].fname}</span>
 									<span class="foodPrice" style="margin-right: 20px;">￥${data.list[i].price}</span>
-									<span class="foodNum">剩余:${data.list[i].number}份</span>
+									<span class="foodNum" style="display:none">剩余:${data.list[i].number}份</span>
 						
 									</h6>
 								</span>
@@ -151,19 +104,20 @@ $(function() {
 		var foodName = $(this).attr("data-foodname"); //食物名称
 		var foodPrice = $(this).attr("data-foodprice");
 		var foodNum = 1; //点击一次加入一次
+		var foodMid = $(this).attr("data-foodmid");//食物所属店铺
 		var product = {
 			foodName: foodName,
 			foodPrice: foodPrice,
 			foodNum: foodNum,
-			foodImg: foodImg
+			foodImg: foodImg,
+			foodMid: foodMid
 		}
 
-		var cartValue = sessionStorage.getItem("cart");
-		
-		if(cartValue != null) {
-			var cartObj = JSON.parse(cartValue); //转为为json对象
-			console.log(cartObj);
-			var item = cartObj.items;
+		console.log(cart);
+		if(sessionStorage.getItem("cart") != null) {
+			cart = JSON.parse(sessionStorage.getItem("cart"));
+			//var cartObj = JSON.parse(cart); //转为为json对象
+			var item = cart.items;
 			var flag = false;
 			$(item).each(function(index, value) {
 				if(product.foodName == value.foodName) {
@@ -173,24 +127,19 @@ $(function() {
 				}
 			})
 			if(flag == false) {
+				console.log(cart);
 				cart.items.push(product);
 			}
 		} else {
+			
 			cart.items.push(product);
 		}
-
+		console.log(cart);
 		var jsonStr = JSON.stringify(cart); //转换为json字符串
 		sessionStorage.setItem("cart", jsonStr); //存入session
 		alert("添加购物车成功！");
 	});
 	
 	
-	//购物车滑过高亮
-	$("#cartImg").on("mouseover",function(){
-		$(this).attr("src","images/carthover.png");
-		
-	})
-	$("#cartImg").on("mouseout",function(){
-		$(this).attr("src","images/cart.png");
-	})
+	
 })
---
+++ src/main/webapp/js/page/order.js
@@ -0,0 +1,77 @@
+/*
+ * 订单
+ */
+
+$(function(){
+	//初始化
+	
+	//获取地址栏参数
+	function GetQueryString(name)
+	{
+	     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+	     var r = window.location.search.substr(1).match(reg);
+	     if(r!=null)return  unescape(r[2]); return null;
+	}
+	var out_trade_no=GetQueryString(GetQueryString);//订单号
+	if(out_trade_no!=null || out_trade_no.length>0){
+		//获取当前时间
+		function getNowFormatDate() {
+	        var date = new Date();
+	        var seperator1 = "-";
+	        var year = date.getFullYear();
+	        var month = date.getMonth() + 1;
+	        var strDate = date.getDate();
+	        if (month >= 1 && month <= 9) {
+	            month = "0" + month;
+	        }
+	        if (strDate >= 0 && strDate <= 9) {
+	            strDate = "0" + strDate;
+	        }
+	        var currentdate = year + seperator1 + month + seperator1 + strDate;
+	        return currentdate;
+	    }
+		var out_trade_no=GetQueryString(GetQueryString);//订单号
+		var userid=sessionStorage.getItem("userid");//userid
+		var createtime=getNowFormatDate();//订单生成时间
+		var remark="暂无备注";//备注
+		var status=1;//订单状态
+		var total=sessionStorage.getItem("foodTotal");//总价
+		var mid=1;
+		function addOrder() {
+			$.ajax({
+				type: "post",
+				url: "http://localhost:8080/smartCanteen/order/addOrder.do",
+				data: {
+					orderid: out_trade_no,
+					createtime: createtime,
+					remark:remark,
+					status:status,
+					total:total,
+					userid:userid,
+					mid:mid
+				},
+				dataType: "json",
+				success: function(data) {
+					if(data==true){
+						console.log("添加订单成功！")
+					}
+				},
+				error: function(error) {
+					console.log(error);
+				}
+			});
+		}
+		addOrder();
+	}
+	
+	function getOrderDetails(){
+		var dom=$("#menu");
+		dom.html(" ");
+		var str="";
+		str=`
+			<h1>暂无订单</h1>
+		`;
+		dom.append(str)
+	};
+	getOrderDetails();
+})
